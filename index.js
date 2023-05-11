5b6e2c6e875e7a88a73b291bdd8a2c8551d0cc59
devvyyxyz <132696257+devvyyxyz@users.noreply.github.com>
2023-05-11 04:20:09 +0000

    edited index.js

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-/* Setup
-
-   1. Create a .env file (click add file then remane it to .env)
-
-   2. Put "token=" (without quotes) into the .env file followed by your Discord Bot token (No spaces!)
-
-*/
-
-/* If you use uptimerobot to ping, delete this line and line 20
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 const express = require("express");
 const app = express();
 
 app.listen(() => console.log("Server started"));
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
 app.use('/ping', (req, res) => {
   res.send(new Date());
 });
 
-*/
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+
+
 
 client.on('message', message => {
   // Here is where you need to code
